b74d0b43d0e05c3d8e8a7b106df34221b21511d2
Jose Torres <jatorres@wonderboxtech.com>
2018-06-20 06:04:56 -0500

    Added new Pages

---
+++ ChurchResourceManagerWeb/Controllers/OfferingsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChurchResourceManagerWeb.Controllers
+{
+    public class OfferingsController : Controller
+    {
+        // GET: Offerings
+        public ActionResult Home()
+        {
+            return View();
+        }
+    }
+}
