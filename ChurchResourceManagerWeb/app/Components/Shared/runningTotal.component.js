0a0efc11aab4666e25b26b20ae07d2343c449d25
Jose Torres <jatorres@wonderboxtech.com>
2018-06-21 06:56:03 -0500

    Added Shared components

---
+++ ChurchResourceManagerWeb/app/Components/Shared/runningTotal.component.js
@@ -0,0 +1,16 @@
+﻿(function () {
+
+    "use strict";
+    angular.module("app")
+        .component("runningTotals",
+            {
+                templateUrl: "/Shared/RunningTotals",
+                controller: "tithesRunningTotalController",
+                controllerAs: "trt",
+                bindings: {
+                    tithesRunningTotal: "<",
+                    getTithesRunningTotal: "&"
+                }
+            });
+
+})();
---
+++ ChurchResourceManagerWeb/app/Controllers/Shared/runningTotalsController.js
@@ -0,0 +1,23 @@
+﻿(function () {
+
+    "use strict";
+    angular.module("app")
+        .controller("tithesRunningTotalController", tithesRunningTotalController);
+
+    tithesRunningTotalController.$inject = [];
+
+    function tithesRunningTotalController() {
+        var vm = this;
+
+        // setup bindable members
+        vm.refreshTotals = refreshTotals;
+
+        ////////////////////////////
+
+
+        function refreshTotals() {
+            vm.getTithesRunningTotal();
+        }
+    }
+
+})();
