86c4656eed898b308593d7a04fda4f9aa0d82bed
Jose Torres <jatorres@wonderboxtech.com>
2018-08-19 08:07:43 -0500

    Added navigation toggle

--- ChurchResourceManagerWeb/app/Controllers/Home/Home.controller.js
+++ ChurchResourceManagerWeb/app/Controllers/Home/Home.controller.js
@@ -3,12 +3,13 @@
     angular.module("app")
         .controller("homeController", homeController);
 
-    homeController.$inject = ["operationFlowService"];
+    homeController.$inject = ["operationFlowService", "enumsDataService"];
 
-    function homeController(operationFlowService) {
+    function homeController(operationFlowService, enumsDataService) {
 
         var vm = this;
 
+        vm.activateDeactivateParent = activateDeactivateParent;
         vm.addMembershipLocationCheck = addMembershipLocationCheck;
         vm.addOfferingsLocationCheck = addOfferingsLocationCheck;
         vm.addTithesLocationCheck = addTithesLocationCheck;
@@ -18,6 +19,17 @@
         activate();
 
         function activate() {
+
+            vm.parentSideBar = {
+                ManageMembership: { active: false },
+                ManageTithes: { active: false },
+                ManageOfferings: { active: false },
+                ManageTransactions: { active: false }
+            };
+        }
+
+        function activateDeactivateParent(name) {
+            vm.parentSideBar[name].active = !vm.parentSideBar[name].active;
         }
 
         function addMembershipLocationCheck() {
